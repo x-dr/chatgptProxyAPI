c859743a08a7603e371fba2f237db70d1f9aef17
x-dr <79109673+x-dr@users.noreply.github.com>
2023-03-02 18:23:27 +0000

    fix

--- demo.js
+++ demo.js
@@ -1,21 +1,3 @@
-
-### 部署到Vercel
-
-1. 创建一个[Vercel](https://vercel.com/)帐户
-2. [Fork 项目](https://github.com/x-dr/chatgptProxyAPI/fork)
-3. 从仪表板页面单击导入项目，然后指定 GitHub 上项目分支的 URL。
-4. 将所需的环境变量添加OPENAI_API_KEY 项目中。
-
-<img src="chatgpt.png" >
-
-5. 点击Deploy部署你的应用程序。
-
-
-### 使用
-<details>
-<summary>使用</summary>
-
-```javaScript
 import axios from 'axios';
 const headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
@@ -64,7 +46,3 @@ res = await requestPromise({
 });
 
 console.log(res.data.text);
-
-```
-
-</details>
