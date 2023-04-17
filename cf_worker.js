14fb0b0ea4384a6e2bb36435ab22247c966e697b
x-dr <79109673+x-dr@users.noreply.github.com>
2023-04-17 17:14:22 +0800

    Update cf_worker.js

--- cf_worker.js
+++ cf_worker.js
@@ -22,16 +22,3 @@ async function handleRequest(request) {
 }
 
 
-// export default {
-//   async fetch(request, env) {
-//     const url = new URL(request.url);
-//     url.host = "api.openai.com";
-//     // openai is already set all CORS heasders 
-//     return fetch(url, {
-//       headers: request.headers,
-//       method: request.method,
-//       body: request.body,
-//       redirect: 'follow'
-//     });
-//   }
-// }
